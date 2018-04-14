009a1dc2ba749b261bd7de8df91098866b0b6db4
00099216 <32801207+00099216@users.noreply.github.com>
2018-04-14 17:57:43 -0600

    first commit

---
+++ app/src/main/java/com/andres00099216/tarea_labo4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andres00099216.tarea_labo4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
